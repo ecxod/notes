f012850adc521bec3c6494ab5680d114fd1004ff
Christian Eichert <c@zp1.net>
2025-05-03 03:23:11 +0200

    modified:   imapkonten.html 	modified:   package.json 	modified:   preload.js

--- preload.js
+++ preload.js
@@ -2,7 +2,8 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
   onSaveNote: (callback) => ipcRenderer.on('save-note', callback),
-  saveNote: (content) => ipcRenderer.invoke('save-note-content', content),
+  //saveNote: (content) => ipcRenderer.invoke('save-note-content', content),
+  saveNote: () => ipcRenderer.send('save-note'),
   loadNotes: () => ipcRenderer.invoke('load-notes'),
   initNotesDir: () => ipcRenderer.invoke('init-notes-dir')
 });
